0513061c8569efa90a7534501b7713afd0a5b6ee
scott93gps <scott93gps@gmail.com>
2017-05-23 16:57:36 -0600

    Example Driver.js is now a searching program that will find which keys are not found in the manifest document's return object.  Still in development.

--- exampleDriver.js
+++ exampleDriver.js
@@ -5,8 +5,13 @@
  * 
  * Written By: Scott Nicholes
  *******************************************/
+var fs = require('fs');
 var manifestSearcher = require('./search-manifest-module.js');
 
-var foundManifest = manifestSearcher('../');
+var foundManifest = manifestSearcher('./');
+var allObject = JSON.parse(fs.readFileSync('allQuickLinksBenCourse.json', 'utf8'));
 
-console.log(foundManifest);
+var allKeys = Object.keys(allObject);
+var manifestKeys = Object.keys(foundManifest);
+
+// Search through the keys and output the ones that are not found in the manifestKeys
